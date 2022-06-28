01b5fcfde7958500ff9c903e804128abb66aeb5f
denisbao <denis.bao@cowmed.com.br>
2022-06-28 10:05:01 -0300

    235 - Using Redux in React Components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,21 @@
-import classes from './Counter.module.css';
+import { useSelector } from 'react-redux'
+
+import classes from './Counter.module.css'
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  // useSelector automaticly make a subscription to the redux store
+  // and will keep this component updated when the state eventualy changes
+  const counter = useSelector(state => state.counter)
+
+  const toggleCounterHandler = () => {}
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
-  );
-};
+  )
+}
 
-export default Counter;
+export default Counter
